daa5699c7a2a338a5c31fb11d0ff5a8a47c7b31e
Pilar Fernández Nieto <rla10309@educastur.es>
2024-02-02 16:14:29 +0100

    live

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const {src, dest, watch} =  require("gulp");
+const {src, dest, watch, parallel} =  require("gulp");
+
+// Dependencias CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Dependencias imágenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
 
 function css(done){
   src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -10,11 +19,54 @@ function css(done){
 
   done(); // Avisa a gulp de que llegamos al final
 }
+function imagenes(done){
+  const opciones = {
+    optimizationLeve: 3
+  }
+    src("src/img/**/*.{png,jpg}")
+      .pipe(cache(imagemin(opciones)))
+      .pipe(dest("build/img"));
+
+  done();
+}
+
+function versionWebp(done){
+  const opciones = {
+    quality: 50
+  }
+  src("src/img/**/*.{png,jpg}")
+  .pipe(webp(opciones))
+  .pipe(dest("build/img"));
+
+  done();
+}
+function versionAvif(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg,jpeg}")
+  .pipe(avif(opciones))
+  .pipe(dest("build/img"));
+
+  done();
+}
+
+function javascript(done){
+  src("src/js/**/*.js")
+  .pipe(dest("build/js"));
+
+  done();
+}
 
 function dev(done){
     watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.javascript = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
---
+++ src/js/app.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+  iniciarApp();
+});
+
+
+function iniciarApp() {
+  navegacionFija();
+  crearGaleria();
+  scrollNav();
+}
+function scrollNav(){
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+  enlaces.forEach(enlace => {
+    enlace.addEventListener("click", (e)=>{
+      const seccionScroll = e.target.attributes.href.value;
+      const seccion = document.querySelector(seccionScroll);
+      seccion.scrollIntoView({
+        behavior: "smooth"
+      });
+
+      
+    })
+  })
+}
+
+function navegacionFija(){
+  const barra = document.querySelector(".header");
+  const contenidoDestacado = document.querySelector(".contenido-destacado");
+  const body = document.querySelector("body");
+
+  window.addEventListener("scroll", () => {
+    if(contenidoDestacado.getBoundingClientRect().top < 0){
+     barra.classList.add("fijo");
+     body.classList.add("body-scroll");
+      
+    }else {
+     barra.classList.remove("fijo");
+     body.classList.remove("body-scroll");
+      
+    }
+  })
+}
+
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="image/avif" />
+        <source srcset="build/img/thumb/${i}.webp" type="image/webp" />
+        <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen-galeria" />
+        `;
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+    galeria.appendChild(imagen);
+  }
+}
+function mostrarImagen(id) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif" />
+        <source srcset="build/img/grande/${id}.webp" type="image/webp" />
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen-galeria" />
+        `;
+
+  // Crea el overlay a la imagen
+  const overlay = document.createElement("div");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+
+  // Cerrar el modal
+  const cerrarModal = document.createElement("p");
+  cerrarModal.textContent = "X";
+  cerrarModal.onclick = function () {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+  cerrarModal.classList.add("btn-cerrar");
+  overlay.append(cerrarModal);
+
+  // lo añade al html
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
+}
