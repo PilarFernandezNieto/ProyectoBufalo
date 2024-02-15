ac53c79938fe7058f6ca45063633b40149d45d7c
Pilar Fernández Nieto <rla10309@educastur.es>
2024-02-15 22:56:33 +0100

    añadido favicon

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>The Electric Buffalo | Inicio</title>
-    <link rel="icon" type="image/x-icon" href="/build/img/favicon.png">
+    <link rel="icon" type="image/x-icon" href="/build/img/favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
