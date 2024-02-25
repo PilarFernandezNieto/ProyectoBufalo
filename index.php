4c3885bb0915db39457e857a5c3458eb9df17234
Pilar Fernández Nieto <rla10309@educastur.es>
2024-02-25 16:07:55 +0100

    premios amas

--- index.php
+++ index.php
@@ -108,11 +108,11 @@ require "includes/funciones.php";
                 <img loading="lazy" width="200" height="300" src="build/img/willy_alvaro_amas.jpg" alt="Premio Amas" title="Alvaro y Wilón en los AMAS" />
             </picture>
             <div class="contenido-noticias">
-                <h3>Patrolman. Premio al mejor disco-rock 2023</h3>
-                <h4>El mejor disco-rock de 2023</h4>
+                <h3>Premio AMAS al mejor disco-rock 2023</h3>
+                <h4>PATROLMAN gana la votación para el premio AMAS a MEJOR
+                    DISCO ROCK 2023.</h4>
                 <div class="texto">
-                    <p>El próximo <b>23 de marzo</b>, The Electric Buffalo se subirá a las tablas de la <b>Factoría Cultural, en Avilés</b>, lugar emblemático por donde los mejores artistas internacionales pasan a mostrar sus trabajos. La banda está encantada de que los promotores hayan querido, en este primer ciclo de conciertos del año, que <b>Patrolman</b> sea presentado allí.</p>
-                    <p> Si no tienes tus entradas, en este enlace puedes obtenerlas:</p>
+                    <p>Muchísimas gracias a todo el mundo que participó en la votación y consideró que nuestro trabajo merecía ser premiado. Nos hace mucha ilusión este premio y, aunque no podemos olvidar que de premios no se vive, sí que ayuda un poco y reconforta el espíritu para continuar la marcha en un mundo musical con una clase trabajadora, es decir, todas las personas que hacen sus canciones en sus locales de ensayo, cada vez más golpeada por las condiciones que impone el mercado.</p>
                 </div>
 
                 <p class="fecha alinear-derecha">24/02/2024</p>
--- premio-amas.php
+++ premio-amas.php
@@ -4,7 +4,7 @@ require "includes/funciones.php";
 ?>
 
 <main class="contenedor seccion5 alto-min contenido-centrado">
-    <h2>Patrolman. Premio al mejor disco-rock 2023</h2>
+    <h2>Premio AMAS al mejor disco-rock 2023</h2>
     <picture>
         <source srcset="build/img/willy_alvaro_amas.webp" type="image/webp" />
         <source srcset="build/img/willy_alvaro_amas.jpg" type="image/jpeg" />
@@ -14,8 +14,73 @@ require "includes/funciones.php";
         <p class="fecha">24/02/2024</p>
 
         <div class="contenido-noticia">
-            <h3>El mejor disco-rock de 2023</h3>
-            <p class="texto"></p>
+            <h3>PATROLMAN gana la votación para el premio AMAS a MEJOR
+                DISCO ROCK 2023</h3>
+            <div class="texto">
+                <p>Muchísimas gracias a todo el mundo que participó en la votación y consideró que nuestro trabajo merecía ser premiado. Nos hace mucha ilusión este premio y, aunque no podemos olvidar que de premios no se vive, sí que ayuda un poco y reconforta el espíritu para continuar la marcha en un mundo musical con una clase trabajadora, es decir, todas las personas que hacen sus canciones en sus locales de ensayo, cada vez más golpeada por las condiciones que impone el mercado.</p>
+                <p>Los efectos ya se están notando claramente en la pelea diaria para
+                    conseguir unas condiciones dignas con algunas salas de conciertos, con
+                    precios de alquiler totalmente desorbitados y con personal que, en
+                    ocasiones, no está cualificado. La responsabilidad del que contrata ese
+                    personal, y no del propio personal -cada uno se gana la vida como puede-,
+                    por sacar el máximo beneficio con la mínima inversión es clave. La forma de
+                    capitalismo más atroz se ha apoderado de este negocio porque,
+                    obviamente, hay posibilidad de negocio. Pero este negocio, curiosamente,
+                    se apoya sobre la voluntad de los propios artistas, músicos y compositores,
+                    sobre su cabezonería por dar forma a sus inquietudes, sean del tipo que
+                    sean, y sacarlas hacia afuera en forma de canciones. Sin esa cabezonería, a
+                    la que algunos románticos llaman <i>amor al arte</i>, no hay nada. Ni música, ni
+                    canciones, ni negocio. Esto hay que repetirlo: sin músicos no hay negocio. Y
+                    es justo decirlo también: no todas las salas son iguales. Ni todos los
+                    promotores. Todavía hay algunas salas y promotores que dan prioridad a
+                    otras cosas antes que al beneficio económico.</p>
+                <p>Las estancias en los hoteles, las comidas fuera de casa, la gasolina
+                    para los desplazamientos... Todo es, en la práctica, casi inabordable
+                    económicamente para que una banda en España, de manera
+                    independiente, pueda conseguir hacerse un público como se hacía hace 20,
+                    30 o 40 años, es decir, a base de carretera. España se ha convertido en un
+                    país preparado para que los turistas se gasten su dinero -cuanto más, mejorla mayor parte del año. Pero los sueldos en el resto de países de la UE, de
+                    donde proceden muchos de esos turistas, no son los sueldos que tenemos
+                    aquí, en España. 1000 euros en Suecia, Dinamarca, Alemania o Francia no
+                    valen lo mismo que aquí, por mucho que nos quieran decir que sí.</p>
+                <p>
+                    A nosotros, la gente trabajadora de España, todo nos cuesta mucho
+                    dinero.
+                </p>
+                <p>Si a todo esto juntamos una legislación laboral en materia
+                    musical/artística del todo anticuada, muy poco flexible y, sobre todo, con
+                    poco contacto con la realidad de la propia actividad musical, la mezcla es
+                    más peligrosa que el tándem Bush-Cheney.</p>
+                <p>
+                    ¿Cuál es el panorama actual? Pues la triste realidad es que, si quieres
+                    trabajar en la música, si quieres intentar hacerte un público y conseguir vivir
+                    <i>casi dignamente</i> de tus canciones, necesitas otro trabajo para poder
+                    INTENTARLO. Nada nuevo bajo el sol, por otra parte, si echamos un vistazo
+                    alrededor: parte de la clase trabajadora en otros sectores sin relación con la
+                    música está en igual situación, empalmando dos -o tres- trabajos de media
+                    jornada o por horas o, en el caso de trabajadores autónomos, trabajando
+                    fines de semana, festivos o con meses sin poder tomarse un descanso. Hay
+                    trabajo, sí, pero se paga mal y a deshora.
+                </p>
+                <p>
+                    En resumen, la clase trabajadora española está en situación precaria,
+                    porque así lo está el trabajo, y el mundo musical no es ajeno a ello. De
+                    nosotros depende intentar contener este asalto.
+                </p>
+                <p>
+                    En fin, para concluir quisiéramos agradecer a todas y cada una de las
+                    personas que acudís no solo a nuestros conciertos, sino de todas las bandas
+                    -sean locales, nacionales o internacionales- que presentan sus trabajos aquí,
+                    que no os resignáis a ver cómo la creatividad se muere para que florezca el
+                    beneficio económico y que, con vuestro grano de arena, por poca cosa que
+                    parezca, aportáis el otro gran soporte sobre el que se asienta todo el 
+                    <i>show business</i>.
+                </p>
+                <p>
+                    Muchas gracias. De corazón.
+                </p>
+                <blockquote class="alinear-derecha">- Álvaro, Tutu, Sam y Wilón - </p>
+            </div>
         </div>
     </div>
     <div class="ms-3 mb-3">
