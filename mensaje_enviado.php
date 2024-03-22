197eaae698e3c3c47d2fa668a9676d8ee5229ade
Pilar Fernández Nieto <rla10309@educastur.es>
2024-03-22 18:58:00 +0100

    Estilos al mensaje enviado

--- mensaje_enviado.php
+++ mensaje_enviado.php
@@ -1,9 +1,11 @@
 <?php
 require "includes/templates/header.php";
 ?>
-<main class="contenedor-formulario alto-min">
-  <h1 class="text-center">Mensaje enviado correctamente</h1>
-  <a href="index.php" class="boton-fireBrick">Volver</a>
+<main class="contenedor-mensaje alto-min">
+  <div>
+      <h1 class="text-center">Mensaje enviado correctamente</h1>
+      <a href="index.php" class="boton-fireBrick">Volver</a>
+  </div>
 
 </main>
 
